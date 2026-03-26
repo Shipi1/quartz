c08758452b29b7f90480c3ab3620dfe5c96ad015
Shipi1 <Shipi1@users.noreply.github.com>
2026-03-26 18:40:55 -0300

    docker: compose

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "your-domain.com/labs",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
