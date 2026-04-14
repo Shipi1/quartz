c6855e47f8a7d0db72afdf3b58f2c904e6174d86
Shipi1 <Shipi1@users.noreply.github.com>
2026-04-13 21:39:40 -0400

    updated base URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "tent.shipisnature.com/",
+    baseUrl: "labs.shipisnature.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
